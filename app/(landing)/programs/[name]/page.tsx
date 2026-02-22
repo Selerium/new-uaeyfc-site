2c25f7d21b0f9306306dd3f851bb20868911be08
John Adithya <jadithya@42student.abudhabi.ae>
2026-02-22 15:14:12 +0400

    adding generatestaticparams to programs/[name]

--- app/(landing)/programs/[name]/page.tsx
+++ app/(landing)/programs/[name]/page.tsx
@@ -1,24 +1,35 @@
 import Link from "next/link";
 
-export default async function SpecificProgram({ params }: {
-    params: {
-        name: string
-    }
-}) {
-    const { name } = await params;
-    const disable = true;
+export async function generateStaticParams() {
+  return [{ name: "big-weekend" }];
+}
 
-    return (
-      <div className="w-full h-fit">
-        {disable && <div className="flex flex-col justify-center items-center gap-4 w-full h-full fixed top-0 left-0 bg-neutral-100 text-black z-50">
-          <h1 className="text-4xl font-semibold">{'THIS PAGE IS UNDER CONSTRUCTION :)'}</h1>
-          <p>{'(sorry about that)'}</p>
-          <Link href='/' className="button">GO TO HOME</Link>
-        </div>}
-        <section className="w-full h-[50dvh] min-h-fit flex flex-col gap-10 justify-center items-center bg-fixed bg-cover bg-bottom bg-brightness-50 bg-[url('/big-weekend.jpg')]">
-        <div className="flex flex-col w-full h-full bg-black/50 justify-center items-center gap-2">
+export default async function SpecificProgram({
+  params,
+}: {
+  params: {
+    name: string;
+  };
+}) {
+  const { name } = await params;
+  const disable = true;
+
+  return (
+    <div className="w-full h-fit">
+      {disable && (
+        <div className="flex flex-col justify-center items-center gap-4 w-full h-full fixed top-0 left-0 bg-neutral-100 text-black z-50">
+          <h1 className="text-4xl font-semibold">
+            {"THIS PAGE IS UNDER CONSTRUCTION :)"}
+          </h1>
+          <p>{"(sorry about that)"}</p>
+          <Link href="/" className="button">
+            GO TO HOME
+          </Link>
         </div>
-        </section>
-      </div>
-    )
+      )}
+      <section className="w-full h-[50dvh] min-h-fit flex flex-col gap-10 justify-center items-center bg-fixed bg-cover bg-bottom bg-brightness-50 bg-[url('/big-weekend.jpg')]">
+        <div className="flex flex-col w-full h-full bg-black/50 justify-center items-center gap-2"></div>
+      </section>
+    </div>
+  );
 }
