20387a7944c81957bb57558bef541da8812fa4e8
Selerium <johnadithya008@gmail.com>
2026-02-26 17:21:53 +0400

    build fail fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: (process.env.staging == 'true') ? '/new-uaeyfc-site' : ''
+  basePath: (process.env.staging) ? '' : '/new-uaeyfc-site'
 };
 
 export default nextConfig;
